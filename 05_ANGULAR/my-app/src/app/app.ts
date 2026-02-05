05ec07eec5133083c7a013d698ab72c84eb3eafa
unknown <mastepalaciosmanuel@gmail.com>
2026-02-05 14:23:33 +0100

    05/02

--- 05_ANGULAR/my-app/src/app/app.ts
+++ 05_ANGULAR/my-app/src/app/app.ts
@@ -1,12 +1,23 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
+  standalone: true, // Requerido para Angular 17+ sin módulos
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App { // El nombre debe coincidir con el que busca main.ts
   protected readonly title = signal('my-app');
+
+  getHeroes() {
+    return [
+      new Pokemon(1, 'Spiderman'),
+      new Pokemon(13, 'Wonder Woman'),
+      new Pokemon(15, 'Lobezno'),
+      new Pokemon(20, 'Catwoman')
+    ];
+  }
 }
